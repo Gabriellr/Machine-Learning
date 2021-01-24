09aff8c8f29707800fe8805d6443a6211da184a3
Gabriel Rodrigues <32747916+Gabriellr@users.noreply.github.com>
2021-01-23 23:58:34 -0300

    Update exerc.py

--- exerc.py
+++ exerc.py
@@ -11,6 +11,13 @@ import pandas as pd
 base = pd.read_csv('credit_data.csv')
 base.describe()
 base.loc[base['age']<0]
+#apagar a somente registros com problemas
 base.drop('age',1,inplace=True)
 base.describe()
 base.drop(base[base.age < 0].index, inplace=True)
+#Preencher os valores manualmente
+#Preencher os valores das medias
+base.mean()
+base['age'].mean()
+base['age'][base.age >0].mean()
+base.loc[base.age < 0, 'age'] =40.92
