03cb53b4f558d88472139f3a3d183abfe80ea810
Gabriel Rodrigues <32747916+Gabriellr@users.noreply.github.com>
2021-01-23 23:51:36 -0300

    Update exerc.py

--- exerc.py
+++ exerc.py
@@ -10,4 +10,7 @@ import pandas as pd
 
 base = pd.read_csv('credit_data.csv')
 base.describe()
-base.loc[base['age']>0]
+base.loc[base['age']<0]
+base.drop('age',1,inplace=True)
+base.describe()
+base.drop(base[base.age < 0].index, inplace=True)
