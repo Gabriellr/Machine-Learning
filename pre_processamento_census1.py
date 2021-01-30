ec7b31c04e03e681210032a865b7d6acd2a5020f
Gabriel Rodrigues <32747916+Gabriellr@users.noreply.github.com>
2021-01-30 18:20:52 -0300

    Create pre_processamento_census1.py

---
+++ pre_processamento_census1.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 24 17:49:29 2021
+
+@author: grodr
+"""
+
+import pandas as pd
+
+
+base = pd.read_csv('census.csv')
+
+previsores = base.iloc[:,0:14].values
+classe = base.iloc[:,14].values
+
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+Labelencoder_previsores = LabelEncoder()
+
+
+
+
+#labels = Labelencoder_previsores.fit_transform(previsores[:,1])
+
+previsores[:,1] = Labelencoder_previsores.fit_transform(previsores[:,1])
+previsores[:,3] = Labelencoder_previsores.fit_transform(previsores[:,3])
+previsores[:,5] = Labelencoder_previsores.fit_transform(previsores[:,5])
+previsores[:,6] = Labelencoder_previsores.fit_transform(previsores[:,6])
+previsores[:,7] = Labelencoder_previsores.fit_transform(previsores[:,7])
+previsores[:,8] = Labelencoder_previsores.fit_transform(previsores[:,8])
+previsores[:,9] = Labelencoder_previsores.fit_transform(previsores[:,9])
+previsores[:,13] = Labelencoder_previsores.fit_transform(previsores[:,13])
+
+
+onehotencoder = OneHotEncoder()
+previsores = onehotencoder.fit_transform(previsores).toarray()
+previsores = previsores[:, 0:]
+
+onehotencorder = ColumnTransformer(transformers=[("OneHot", OneHotEncoder(), [1,3,5,6,7,8,9,13])],remainder='passthrough')
+
+labelencoder_classe = LabelEncoder()
+classe = labelencoder_classe.fit_transform(classe)
