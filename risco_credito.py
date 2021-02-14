d5bf8737c7df85b7d1cd97cc6f7e25b132d98a16
Gabriel Rodrigues <32747916+Gabriellr@users.noreply.github.com>
2021-02-14 17:40:02 -0300

    Update risco_credito.py

--- risco_credito.py
+++ risco_credito.py
@@ -14,12 +14,13 @@ classe = base.iloc[:,4].values
 
 from sklearn.preprocessing import LabelEncoder
 labelencoder = LabelEncoder()
-previsores[:,0] = labelencoder.fit(previsores[:,0])
-previsores[:,0] = labelencoder.fit(previsores[:,0])
-previsores[:,0] = labelencoder.fit(previsores[:,0])
-previsores[:,0] = labelencoder.fit(previsores[:,0])
-previsores[:,0] = labelencoder.fit(previsores[:,0])
+previsores[:,0] = labelencoder.fit_transform(previsores[:,0])
+previsores[:,1] = labelencoder.fit_transform(previsores[:,1])
+previsores[:,2] = labelencoder.fit_transform(previsores[:,2])
+previsores[:,3] = labelencoder.fit_transform(previsores[:,3])
 
 from sklearn.naive_bayes import GaussianNB
 classificador = GaussianNB()
 classificador.fit(previsores, classe)
+
+resultado = classificador.predict([[0,0,1,2]])
